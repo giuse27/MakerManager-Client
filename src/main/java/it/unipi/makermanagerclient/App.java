d5ae84a0ee501755a6b94d927595171b67c7ad1c
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-05 14:42:56 +0200

    rinominata l'app in MakerManager e modficata la dimensione della finestra di avvio

--- src/main/java/it/unipi/makermanagerclient/App.java
+++ src/main/java/it/unipi/makermanagerclient/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("main"), 720, 640);
+        stage.setTitle("MakerManager");
         stage.setScene(scene);
         stage.show();
     }
