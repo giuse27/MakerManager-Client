1533c51550b21153b6bb59e9bc0ce0c194e97055
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-05 16:08:24 +0200

    comunicazione http con il server

--- src/main/java/it/unipi/makermanagerclient/network/ApiClient.java
+++ src/main/java/it/unipi/makermanagerclient/network/ApiClient.java
@@ -1,8 +1,10 @@
 package it.unipi.makermanagerclient.network;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.Duration;
 
 /**
@@ -27,41 +29,75 @@ public final class ApiClient {
     /**
      * Esegue una get
      * 
-     * @param url
-     * @return
-     * @throws IOException
-     * @throws InterruptedException
+     * @param url url della chiamata
+     * @return restituisce il body della risposta come stringa
+     * @throws IOException se la richiesta fallisce (es. server non raggiungibile)
+     * @throws InterruptedException se viene interrotto durante l'attesa
      */
     public static ApiResponse get(String url) 
             throws IOException, InterruptedException 
     {
 
-        return null;
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(url))
+                                .GET()
+                                .build();
+
+        return invia(request);
 
     }
 
     /**
      * Esegue una post
      * 
-     * @param url
-     * @param corpoJson
-     * @return
-     * @throws IOException
-     * @throws InterruptedException
+     * @param url url della chiamata
+     * @param corpoJson json serializzato come stringa
+     * @return restituisce il body della risposta come stringa
+     * @throws IOException se la richiesta fallisce (es. server non raggiungibile)
+     * @throws InterruptedException se viene interrotto durante l'attesa
      */
     public static ApiResponse post(String url, String corpoJson) 
             throws IOException, InterruptedException 
     {
+    
+        // creo il body vero e proprio
+        HttpRequest.BodyPublisher body;
 
-        return null;
+        // lo costruisco e se il json è vuoto (es per /inizializza) lo creo vuoto
+        if (corpoJson == null || corpoJson.isBlank()) {
+            body = HttpRequest.BodyPublishers.noBody();
+        } else {
+            body = HttpRequest.BodyPublishers.ofString(corpoJson);
+        }
+
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(url))
+                                .header("Content-Type", "application/json")
+                                .POST(body)
+                                .build();
+
+        return invia(request);
 
     }
 
+    /**
+     * esegue una delete
+     * 
+     * @param url url della chiamata
+     * @return restituisce il body della risposta come stringa
+     * @throws IOException se la richiesta fallisce (es. server non raggiungibile)
+     * @throws InterruptedException se viene interrotto durante l'attesa
+     */
     public static ApiResponse delete(String url) 
             throws IOException, InterruptedException 
     {
 
-        return null;
+        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(URI.create(url))
+                                .DELETE()
+                                .build();
+
+        return invia(request);
 
     }
 
@@ -69,15 +105,20 @@ public final class ApiClient {
      * Metodo di utilità privata per ridurre la ridonanza
      * 
      * @param richiesta
-     * @return
-     * @throws IOException
-     * @throws InterruptedException
+     * @return Resituisce un record con lo status code e il body di risposta
+     * @throws IOException se la richiesta fallisce (es. server non raggiungibile)
+     * @throws InterruptedException se viene interrotto durante l'attesa
      */
     private static ApiResponse invia(HttpRequest richiesta) 
             throws IOException, InterruptedException 
     {
 
-        return null;
+        HttpResponse<String> response = HTTP_CLIENT.send(
+            richiesta,
+            HttpResponse.BodyHandlers.ofString()    
+        );
+
+        return new ApiResponse(response.statusCode(), response.body());
 
     }
 
