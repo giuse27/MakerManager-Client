b75efb474ea19998df7b53dca419c70d018f1e01
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-05 15:37:53 +0200

    Definizione dei metodi di ApiClient

--- src/main/java/it/unipi/makermanagerclient/network/ApiClient.java
+++ src/main/java/it/unipi/makermanagerclient/network/ApiClient.java
@@ -1,6 +1,8 @@
 package it.unipi.makermanagerclient.network;
 
+import java.io.IOException;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.time.Duration;
 
 /**
@@ -20,6 +22,63 @@ public final class ApiClient {
         throw new UnsupportedOperationException(
             "ApiClient è una classe di utilità statica e non può essere istanziata."
         );
+    }
+
+    /**
+     * Esegue una get
+     * 
+     * @param url
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static ApiResponse get(String url) 
+            throws IOException, InterruptedException 
+    {
+
+        return null;
+
+    }
+
+    /**
+     * Esegue una post
+     * 
+     * @param url
+     * @param corpoJson
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static ApiResponse post(String url, String corpoJson) 
+            throws IOException, InterruptedException 
+    {
+
+        return null;
+
+    }
+
+    public static ApiResponse delete(String url) 
+            throws IOException, InterruptedException 
+    {
+
+        return null;
+
+    }
+
+    /**
+     * Metodo di utilità privata per ridurre la ridonanza
+     * 
+     * @param richiesta
+     * @return
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private static ApiResponse invia(HttpRequest richiesta) 
+            throws IOException, InterruptedException 
+    {
+
+        return null;
+
     }
 
 }
