f2a14c9d08af8f97c5978eca8a8e093ce339054b
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-08 23:51:54 +0200

    aggiornato il module info con json e i nuovi package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,10 @@ module it.unipi.makermanagerclient {
     // esterna scaricata da Maven.
     requires java.net.http;
 
+    // Libreria usata da GsonProvider per (de)serializzare i corpi JSON
+    // scambiati con il Server
+    requires com.google.gson;
+
     // "opens ... to javafx.fxml" concede a FXMLLoader accesso via
     // reflection alle classi di questo package: e' necessario per ogni
     // package che contiene classi Controller (con campi/metodi @FXML),
@@ -17,6 +21,13 @@ module it.unipi.makermanagerclient {
     // accesso, anche se il codice compila correttamente.
     opens it.unipi.makermanagerclient to javafx.fxml;
     opens it.unipi.makermanagerclient.controller to javafx.fxml;
+
+    // "opens ... to com.google.gson" concede a Gson accesso via
+    // reflection ai campi PRIVATI dei DTO in questo package. Gson
+    // per default legge/scrive direttamente i campi delle classi che
+    // (de)serializza, e questo richiede "opens" anche se i campi hanno
+    // gia' getter/setter pubblici.
+    opens it.unipi.makermanagerclient.model to com.google.gson;
 
     // "exports" rende il package visibile dall'esterno del modulo. Va
     // dichiarato per ogni package che contiene classi referenziate da
@@ -28,5 +39,7 @@ module it.unipi.makermanagerclient {
     exports it.unipi.makermanagerclient.config;
     exports it.unipi.makermanagerclient.network;
     exports it.unipi.makermanagerclient.util;
+    exports it.unipi.makermanagerclient.sessione;
+    exports it.unipi.makermanagerclient.service;
 
 }
