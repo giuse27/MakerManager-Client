2350cb154b1ce70da541b38d0eab44d3f502843d
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-09 16:55:39 +0200

    struttura base del controller per auth.fxml

--- src/main/java/it/unipi/makermanagerclient/controller/AuthController.java
+++ src/main/java/it/unipi/makermanagerclient/controller/AuthController.java
@@ -1,5 +1,53 @@
 package it.unipi.makermanagerclient.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class AuthController {
+
+    @FXML
+    private TextField inserisciEmailAccedi;
     
+    @FXML
+    private TextField inserisciPasswordAccedi;
+
+    @FXML
+    private Label etichettaErroreAccedi;
+
+    @FXML
+    private Button pulsanteAccedi;
+
+    @FXML
+    private TextField inserisciNicknameRegistrati;
+
+    @FXML
+    private TextField inserisciEmailRegistrati;
+    
+    @FXML
+    private TextField inserisciPasswordRegistrati;
+
+    @FXML
+    private Label etichettaErroreRegistrati;
+
+    @FXML
+    private Button pulsanteRegistrati;
+
+    /**
+     * pressione del tasto Accedi
+     */
+    @FXML
+    private void onAccedi() {
+
+    }
+
+    /**
+     * Pressione del tasto registrati
+     */
+    @FXML
+    private void onRegistrati() {
+
+    }
+
 }
